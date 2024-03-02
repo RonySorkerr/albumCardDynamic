6a669e36fcf9e6890c604cd6a61ae6585d61a992
RonySorkerr <mdronysarkar33299@gmail.com>
2024-03-02 12:21:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const loadAPI = async () => {
+    const url = 'https://jsonplaceholder.org/users'
+    const res = await fetch(url)
+    const data = await res.json()
+    loadData(data)
+}
+
+const loadData = (data) => {
+    const cardContainer = document.getElementById('cardContainer')
+    data.forEach(users => {
+        
+        // console.log(users)
+        const userCard = document.createElement('div')
+        userCard.classList = `card bg-base-100 shadow-xl`
+        userCard.innerHTML = `
+        <div class="card-body text-center">
+        <h2 class="text-center text-2xl font-extrabold">${users.firstname} ${users.lastname}</h2>
+        <p>${users.website}</p>
+        <p></p>
+        <p></p>
+        <div class="card-actions justify-center"><button class="btn btn-primary" onclick=""fullInfo()">Full Info</button></div>
+        </div>
+        `
+        cardContainer.appendChild(userCard)
+    });
+}
+
+const fullInfo = () =>{
+    console.log('clicked')
+}
+
+loadAPI()
